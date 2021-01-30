70c8d3eac1589b2d799ff6e6f9f3ed166b01c8a3
unknown <kweperera93111@gmail.com>
2021-01-30 23:53:02 +0530

    Update Service

--- src/main/java/com/satellite_api_master/satellite_monitor/controller/SatelliteMonitor.java
+++ src/main/java/com/satellite_api_master/satellite_monitor/controller/SatelliteMonitor.java
@@ -23,16 +23,17 @@ public class SatelliteMonitor {
     RestTemplate restTemplate;
 
     @RequestMapping(value = "monitorSatellite/{id}/{url}/{url2}")
-    public String monitorSatellite(@PathVariable("id") Long id, @PathVariable ("url") String url, @PathVariable ("url2") String url2) {
-        String satellite_data ="";
-        String launch_data ="";
+    public String monitorSatellite(@PathVariable("id") Long id, @PathVariable("url") String url, @PathVariable("url2") String url2) {
+        String catalog_data = "";
+        String location_data = "";
         HttpHeaders headers = new HttpHeaders();
         headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
         HttpEntity<String> entity = new HttpEntity<String>(headers);
-        String catalog_url = String.format("http://%s:8089/getCatalogData/%x",url,id);
-        String location_url = String.format("http://%s:8087/getLocationDataById/%x",url2,id);
-        satellite_data = restTemplate.exchange(catalog_url, HttpMethod.GET, entity, String.class).getBody();
-        launch_data = restTemplate.exchange(location_url, HttpMethod.GET, entity, String.class).getBody();
-        return(satellite_data + " " + launch_data);
+        String catalog_url = String.format("http://%s:8089/getCatalogData/%x", url, id);
+        String location_url = String.format("http://%s:8087/getLocationDataById/%x", url2, id);
+        catalog_data = restTemplate.exchange(catalog_url, HttpMethod.GET, entity, String.class).getBody();
+        location_data = restTemplate.exchange(location_url, HttpMethod.GET, entity, String.class).getBody();
+        return (catalog_data + " " + location_data);
     }
+
 }
